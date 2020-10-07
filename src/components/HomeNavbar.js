f5a92e9ef56e7dc186a52229ebcca6b31b8f446a
CornetS28 <corsam28@gmail.com>
2020-10-07 15:14:48 -0400

    add link/button in the navbar

--- src/components/HomeNavbar.js
+++ src/components/HomeNavbar.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Link from 'react-router-dom/Link';
+
+// MUI stuff
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+// import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 class HomeNavbar extends Component {
   render() {
     return (
-      <div>
-        Start Writing
-      </div>
-    )
+      <AppBar>
+        <Toolbar className="home-nav-container">
+          <Button color="inherit" component={Link} to="/editor-preview">Start Writing</Button>
+        </Toolbar>
+      </AppBar>
+    );
   }
 }
 
--- src/pages/markdownEditorAndPreview.js
+++ src/pages/markdownEditorAndPreview.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+// Components
+import MarkdownAndPreviewNavbar from "../components/MarkdownAndPreviewNavbar";
+
 class markdownEditoAndPreview extends Component {
   render() {
     return (
       <div>
+        <MarkdownAndPreviewNavbar />
         <h1>Hello from markdownEditoAndPreview</h1>
       </div>
     );
