42137c001f33bb7407097f8c034d77e8b8f0598b
CornetS28 <corsam28@gmail.com>
2020-10-07 12:59:54 -0400

    create some basic pages

---
+++ src/pages/markdownEditorAndPreview.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class markdownEditoAndPreview extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from markdownEditoAndPreview</h1>
+      </div>
+    );
+  }
+}
+
+export default markdownEditoAndPreview;
