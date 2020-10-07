b0b6471e645eb5508085f5132c37e79e843f03e3
CornetS28 <corsam28@gmail.com>
2020-10-07 13:05:02 -0400

    add some basic components

---
+++ src/components/HomeNavbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HomeNavbar extends Component {
+  render() {
+    return (
+      <div>
+        Start Writing
+      </div>
+    )
+  }
+}
+
+export default HomeNavbar;
