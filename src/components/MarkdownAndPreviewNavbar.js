777121c9d8e7c10f72b0bce13e77de552388f704
CornetS28 <corsam28@gmail.com>
2020-10-07 15:16:08 -0400

    create  MarkdownAndPreviewNavbar

--- src/components/MarkdownAndPreviewNavbar.js
+++ src/components/MarkdownAndPreviewNavbar.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+// MUI stuff
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+// import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 export class MarkdownAndPreviewNavbar extends Component {
   render() {
     return (
       <div>
-        
+        <AppBar>
+          <Toolbar>
+            {/* TODO */}
+            <Button color="inherit">Markdown Icons goes here</Button>
+            <Button color="inherit">Upload GitHub File</Button>
+          </Toolbar>
+        </AppBar>
       </div>
-    )
+    );
   }
 }
 
-export default MarkdownAndPreviewNavbar
+export default MarkdownAndPreviewNavbar;
