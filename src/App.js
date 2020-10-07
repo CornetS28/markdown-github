52e3e335057182c59208c4be2d2cc9add018e4ef
CornetS28 <corsam28@gmail.com>
2020-10-07 13:00:41 -0400

    add routes for the respective pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+// Pages
+import home from './pages/home';
+import markdownEditorAndPreview from "./pages/markdownEditorAndPreview";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route
+              exact
+              path="/editor-preview"
+              component={markdownEditorAndPreview}
+            />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
+
