e3e28946718d46f748f3f36564c8130263d4788f
CornetS28 <corsam28@gmail.com>
2020-10-11 16:17:02 -0400

    hotfix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Logout from './logout';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Logout />
   </React.StrictMode>,
   document.getElementById('root')
 );
