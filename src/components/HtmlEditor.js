a3c49415ae9ee5015c5a60a9100b9158fb9a4c57
sammychinedu2ky <sammychinedu2ky@gmail.com>
2020-10-11 14:13:53 +0100

    added HtmlEditor

---
+++ src/components/HtmlEditor.js
@@ -0,0 +1,11 @@
+import React,{useContext} from 'react'
+import {Context} from '../pages/markdownEditorAndPreview'
+
+
+function HtmlEditor(){
+let {state:content} = useContext(Context)
+
+return <div  dangerouslySetInnerHTML={{__html: content}}/>
+}
+
+export default HtmlEditor
