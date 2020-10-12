2dcb0846d7a60edf92deb5c0d58e386ce8802f7a
CornetS28 <corsam28@gmail.com>
2020-10-12 05:49:50 -0400

    transform file to mardown

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const markdownEditor = `Write or edit your markdown here...
+import ReactMarkdown from "react-markdown";
 
+import MarkdownReadme from './MarkdownReadme'
+
+
+const markdownEditor = `# h1 Heading 8-)
+ 
+**This is some bold text!**
+ 
+This is normal text
+   ${MarkdownReadme}
 `;
+
+
+
 
 export default markdownEditor;
---
+++ src/components/MarkdownReadme.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+
+class MarkdownViewer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: null,
+      urlGitHubFile:
+        "https://github.com/CornetS28/markdown-github/blob/master/README.md",
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(this.state.urlGitHubFile)
+      .then((response) => {
+        console.log(
+          "Success in fetching the file from " + this.state.urlGitHubFile
+        );
+        this.setState({ content: response.data });
+      })
+      .catch((error) => {
+        console.err(
+          "Error in fetching the file from " + this.state.urlGitHubFile
+        );
+      });
+  }
+
+  render() {
+    const { urlGitHubFile, content } = this.state;
+
+    return (
+      <div>
+        <h3> Fetched from: </h3> {urlGitHubFile}
+        <hr />
+        <p>
+          <ReactMarkdown source={content} />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default MarkdownViewer;
